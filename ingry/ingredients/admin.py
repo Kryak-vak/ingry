2e4796268c3877ebac1e986418ef84eee441fda4
kryak_vak <mr.vakashev@mail.ru>
2024-10-24 19:16:29 +0300

    added ingredients image to admin panel

--- ingry/ingredients/admin.py
+++ ingry/ingredients/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Ingredient
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     ordering = ['name', 'id']
-    list_display = ('name', 'id', 'category')
+    list_display = ('name', 'ingredient_image', 'id', 'category')
     list_display_links = ('name', 'id')
     list_editable = ('category', )
+
+    @admin.display(description='Картинка')
+    def ingredient_image(self, ingredient: Ingredient):
+        return mark_safe(f'<img src="{ingredient.image.url}" width=80>')
 
